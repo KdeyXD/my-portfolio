515e578a4e17b2020fb877ca08e519a61c13a6de
Sopheakdey Chea <sopheakdeychea2502@gmail.com>
2025-07-19 02:15:56 +0700

    Enhance Footer layout and add skills section to Home page

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-indigo-950 text-white flex p-5 justify-between items-center'>
+    <div className='bg-indigo-950 text-white flex flex-col md:flex-row p-5 justify-between items-center'>
         <p>&copy;2025 DevPortfolio. All rights reserved.</p>
       <p>Make with React + Tailwind</p>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,19 @@ import { Github, Mail, Linkedin } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const skills = [
+    "React",
+    "Node.js",
+    "JavaScript",
+    "TypeScript",
+    "PHP",
+    "Database",
+    "Tailwind",
+    "Java",
+  ];
   return (
-    <main>
-      <section className="h-[90dvh] text-white px-5">
+    <main className="text-white">
+      <section className="h-[90dvh] px-5">
         <div className="container m-auto flex flex-col-reverse md:flex-row items-center justify-center min-h-full md:gap-10">
           {/* Text Content */}
           <div className=" text-center md:text-left">
@@ -19,7 +29,10 @@ const Home = () => {
 
             {/* Buttons */}
             <div className="mt-6 flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
-              <Link to='/contact' className="px-5 py-2 rounded-md bg-blue-600 hover:bg-blue-700 transition">
+              <Link
+                to="/contact"
+                className="px-5 py-2 rounded-md bg-blue-600 hover:bg-blue-700 transition"
+              >
                 Get in touch
               </Link>
               <button className="px-5 py-2 rounded-md border border-slate-300 hover:bg-slate-700 transition">
@@ -29,13 +42,22 @@ const Home = () => {
 
             {/* Socials */}
             <div className="mt-6 flex justify-center md:justify-start gap-4">
-              <Link to="/" className="bg-slate-700 p-3 rounded-lg hover:bg-slate-600">
+              <Link
+                to="/"
+                className="bg-slate-700 p-3 rounded-lg hover:bg-slate-600"
+              >
                 <Github className="w-5 h-5" />
               </Link>
-              <Link to="/" className="bg-slate-700 p-3 rounded-lg hover:bg-slate-600">
+              <Link
+                to="/"
+                className="bg-slate-700 p-3 rounded-lg hover:bg-slate-600"
+              >
                 <Linkedin className="w-5 h-5" />
               </Link>
-              <Link to="/" className="bg-slate-700 p-3 rounded-lg hover:bg-slate-600">
+              <Link
+                to="/"
+                className="bg-slate-700 p-3 rounded-lg hover:bg-slate-600"
+              >
                 <Mail className="w-5 h-5" />
               </Link>
             </div>
@@ -53,8 +75,26 @@ const Home = () => {
       </section>
 
       {/* feature projects */}
-      <section className="h-dvh bg-indigo-900">Hi</section>
-      <section className="h-dvh bg-indigo-900">Hi</section>
+      <section className=" bg-indigo-900 p-5">
+        <div className="text-center">
+        <h1 className=" text-4xl font-bold mb-3">Feature Projects</h1>
+        <p>No project avaliable</p>
+        </div>
+        <div className="text-center mt-3 p-5">
+            <Link to='/projects' className="rounded-lg px-4 py-3 bg-blue-500">View more project &gt;&gt;&gt;</Link>
+        </div>
+      </section>
+      <section className="p-5">
+        <h1 className="text-center text-4xl font-bold">Skills & Technologies</h1>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 p-5 container m-auto text-center">
+        {skills.map((skill, i) => (
+            <div className="bg-slate-800 p-5 rounded-lg" key={i}>{skill}</div>
+          ))}
+          </div>
+          <div className="text-center">
+            <button className="rounded-lg px-4 py-3 bg-blue-500">Learn more about me &gt;&gt;&gt;</button>
+          </div>
+      </section>
     </main>
   );
 };
