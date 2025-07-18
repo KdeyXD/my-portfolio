a2f8cdff79ffc548690f967c19dcd6846a3bffee
Sopheakdey Chea <sopheakdeychea2502@gmail.com>
2025-07-19 02:19:13 +0700

    Refactor loading fallback to use a dedicated Loading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, {Suspense} from 'react'
 import {Routes, Route} from 'react-router-dom'
 import { routes } from './routes'
-
+import Loading from './components/Loading'
 const App = () => {
   const {path, element, children} = routes
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading />}>
       <Routes>
         <Route path={path} element={element}>
           {children.map((child, i) =>(
---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='bg-gradient-to-tr from-blue-500 via-indigo-700 to-slate-900 h-dvh text-white flex justify-center items-center'>
+        <h1 className='font-bold text-xl'>Loading...</h1>
+    </div>
+  )
+}
+
+export default Loading
